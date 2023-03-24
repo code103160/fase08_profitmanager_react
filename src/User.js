5be9753e0b7f03f2597ea536cb44a92f93f71eac
unknown <lucacscoutinho@gmail.com>
2023-03-24 17:30:17 -0300

    finalização da refatoração

--- src/User.js
+++ src/User.js
@@ -34,129 +34,115 @@ export function UserForm() {
           password_confirmation: password_confirmation
         }
       ),
-
-
     
       success: function(resposta){
         console.log("Sucesso!");
         console.log(resposta);
 
-        $.each(resposta.data,function(index,value){
-
-          guardaDados[index] = value;
-
+        var obj = guardaDados;
+        $.each(resposta.data, function(index,value){
+          obj[index] = value;
         });
+        setGuardaDados(obj);
 
-      setTimeout(function() {
+        setTimeout(function() {
+          var novaLista = lista;
+          novaLista.push(guardaDados);
 
-        var novaLista = this.state.lista;
-        novaLista.push(this.guardaDados);
-        //this.setState({lista:novaLista});
+          PubSub.publish('atualiza-lista-usuarios', novaLista);
+          alert("Cadastro efetuado com Sucesso!")
+          setNome('');
+          setEmail('');
+          setPassword('');
+          setPasswordConfirmation('');
+          setGuardaDados({});
+          setLista(novaLista);
+        }, 10);
+      },
 
-        PubSub.publish('atualiza-lista-usuarios', novaLista);
-        alert("Cadastro efetuado com Sucesso!")
-        this.setState = {
-          name: '',
-          email: '',
-          password: '',
-          password_confirmation: '' 
+      complete: function(resposta){
+        console.log("Complete!!");
+        console.log(resposta.getAllResponseHeaders());
+        
+        var obj = guardaDados;
+        obj.token = resposta.getAllResponseHeaders('access-token');
+        obj.client = resposta.getAllResponseHeaders('client');
+        obj.uid = resposta.getAllResponseHeaders('uid');
+        setGuardaDados(obj);
+      },
+
+      error: function(resposta){ 
+        if(resposta.status === 422){
+          new ManageErrors().publishErrors(resposta.responseJSON);
         }
-
-              this.guardaDados = {};
-
-            }.bind(this), 10);
-
-          }.bind(this),
-
-          complete: function(resposta){
-            console.log("Complete!!");
-            console.log(resposta.getAllResponseHeaders());
-            
-            this.guardaDados.token = resposta.getResponseHeader('access-token');
-            this.guardaDados.client = resposta.getResponseHeader('client');
-            this.guardaDados.uid = resposta.getResponseHeader('uid');
-          },
-
-          error: function(resposta){
-            
-            if(resposta.status === 422){
-              new ManageErrors().publishErrors(resposta.responseJSON);
-            }
-
-          }
-        });
       }
+    });
+
+  }
     
 
-        return(
+  return(
 
-            <div>						
-                <h1 class="h2">Cadastro de Usuários</h1>						
-                <form method="post" onSubmit={this.enviaForm}>
+    <div>						
+      <h1 class="h2">Cadastro de Usuários</h1>						
+      <form method="post" onSubmit={enviaForm}>
                   
-                  <InputCustomizado type="text" id="name" name="name" placeholder="Nome" label="Name" value={nome} onChange={e => setNome(e.target.value)}  />
+        <InputCustomizado type="text" id="name" name="name" placeholder="Nome" label="Name" value={nome} onChange={e => setNome(e.target.value)}  />
 
-                  <InputCustomizado type="email" id="email" name="email" placeholder="E-mail" label="E-mail" value={email} onChange={e => setEmail(e.target.value)}/>
+        <InputCustomizado type="email" id="email" name="email" placeholder="E-mail" label="E-mail" value={email} onChange={e => setEmail(e.target.value)}/>
 
-                  <InputCustomizado type="password" id="password" name="password" placeholder="Senha" label="Password" value={password} onChange={e => setPassword(e.target.value)}/>
+        <InputCustomizado type="password" id="password" name="password" placeholder="Senha" label="Password" value={password} onChange={e => setPassword(e.target.value)}/>
 
-                  <InputCustomizado type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirme a Senha" label="Password Confirmation" value={password_confirmation} onChange={e => setPasswordConfirmation(e.target.value)}/>
+        <InputCustomizado type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirme a Senha" label="Password Confirmation" value={password_confirmation} onChange={e => setPasswordConfirmation(e.target.value)}/>
 
-                  <button type="submit" class="btn btn-primary">Inscrever-se</button>
-                </form>						
-            </div>
+          <button type="submit" class="btn btn-primary">Inscrever-se</button>
+        </form>						
+    </div>
 
-        );
-
-    }
+  );
 
 }
 
-export class UserTable extends Component{ 
-
-  constructor() {
-    super();
-    this.state = {lista : []};
-  }
-
-  componentDidMount(){
-    PubSub.subscribe('atualiza-lista-usuarios', function(topico, novaLista){
-      this.setState({lista:novaLista});
-    }.bind(this))
+export function UserTable() { 
+  const [lista, setLista] = useState([]);
+  
+  useEffect(function() {
+    PubSub.subscribe('atualiza-lista-usuarios', function(topico, novaLista) {
+      console.log("Nova Lista!");
+      setLista(novaLista);
+    });
 
     PubSub.subscribe('erro-validacao', function(topico, erro){
       alert(erro);
-    })
-  }
+    });
+  });
 
-  render(){
-    return(
-      <div class="table-responsive">
-        <h2>Usuários</h2>
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>E-mail</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.state.lista.map(function(user){
-                return(
-                  <tr>
-                    <td>{user.id}</td>
-                    <td>{user.nome}</td>
-                    <td>{user.email}</td>
-                  </tr>
-                );
-              })
-            }							
-          </tbody>
-        </table>
-      </div> 
-    );
-  }
 
+  return(
+    <div class="table-responsive">
+      <h2>Usuários</h2>
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>E-mail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            lista.map(function(user){
+              return(
+                <tr>
+                  <td>{user.id}</td>
+                  <td>{user.nome}</td>
+                  <td>{user.email}</td>
+                </tr>
+              );
+            })
+          }							
+        </tbody>
+      </table>
+    </div> 
+  );
 }
